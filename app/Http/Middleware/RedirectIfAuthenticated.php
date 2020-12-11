a6e7a892ae7626c59283016c249902c38b67f514
ShunyaSuzuki <anyushu2017@gmail.com>
2020-12-11 11:06:58 +0900

    add imgs

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,9 +19,12 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            if ($guard == 'manage') {
+                return redirect(RouteServiceProvider::MANAGE_HOME);
+            } else {
+                return redirect(RouteServiceProvider::HOME);
+            }
         }
-
         return $next($request);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] != null) {
+            $url = $_SERVER['HTTP_HOST'];
+            $domain_array = explode('.', $url);
+            $sub_domain = $domain_array[0];
+            $manages = DB::table('manages')->where('domain', $sub_domain)->first();
+
+            if ($manages !== null) {
+                if (session('receipt.shop_id') != null) {
+                    $manages_shops = DB::table('shops')->find(session('receipt.shop_id'));
+                } else {
+                    $manages_shops = DB::table('shops')->where('manages_id', $manages->id)->first();
+                }
+                $title = $manages->name;
+                $description = $manages->description;
+
+                View::share([
+                    'sub_domain' => $sub_domain,
+                    'meta_title' => $title,
+                    'meta_description' => $description,
+                    'manages' => $manages,
+                    'manages_shops' => $manages_shops,
+                ]);
+            } else {
+                abort('404');
+            }
+        }
     }
 }
